8ef66385292306f5a54a6351e0addd7b73ac00e1
Emanuele <s1116727@studenti.univpm.it>
2025-07-17 11:31:54 +0200

    log postgres su splunk

---
+++ splunk/custom-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Fix permessi su directory dei log condivisa con Postgres
+if [ -d "/mnt/postgres_logs" ]; then
+    echo "[INFO] Setting permissions on /mnt/postgres_logs"
+    chmod -R a+rX /mnt/postgres_logs || true
+    chmod -R a+rX /mnt/postgres_logs/postgresql.log || true
+    echo "[INFO] Setted permissions on /mnt/postgres_logs"
+fi
+
+# ✅ Avvia Splunk normalmente
+exec /sbin/entrypoint.sh start
