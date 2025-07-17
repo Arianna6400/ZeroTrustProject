027dcdb5e98c6c88d85ca4d3bb9fdc39226a46ee
Emanuele <s1116727@studenti.univpm.it>
2025-07-17 12:47:02 +0200

    permessi log snort

--- splunk/custom-entrypoint.sh
+++ splunk/custom-entrypoint.sh
@@ -9,7 +9,12 @@ if [ -d "/mnt/postgres_logs" ]; then
     echo "[INFO] Setted permissions on /mnt/postgres_logs"
 fi
 
-chown -R splunk:splunk /opt/splunk
+if [ -d "/mnt/snort_logs" ]; then
+    echo "[INFO] Setting permissions on /mnt/snort_logs"
+    chmod -R a+rX /mnt/snort_logs || true
+    chmod -R a+rX /mnt/snort_logs/*  || true
+    echo "[INFO] Setted permissions on /mnt/snort_logs"
+fi
 
 # ✅ Avvia Splunk normalmente
 exec /sbin/entrypoint.sh start
